d6eb213c8c7508cf6f222306a04c13c9753e02c1
Anna Wu <shinshinwu@gmail.com>
2015-03-13 18:11:34 -0700

    few bugs need to debug, but mostly functioning

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -18,7 +18,7 @@ class AnswersController < ApplicationController
 
   def show
     @question = Question.find(params[:question_id])
-    @answer = Answer.find(params[:answer_id])
+    @answer = Answer.find(params[:id])
   end
 
   def upvote
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,14 +2,6 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
 
-  def new
-    @question = Question.find(params[:id])
-    @answer = Answer.new
-  end
-
-  def create
-  end
-
   def upvote
     self.votes += 1
     self.save
